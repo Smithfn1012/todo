ea601e8d18b83040432c192066f89a7ec26b7566
Smithfn1012 <58006468+Smithfn1012@users.noreply.github.com>
2020-05-08 18:56:37 -0500

    config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :todo_list do
-    resources :todo_items
-  end 
-  
-  root "todo_list#index"
+    resources :todo_items do
+     member do
+      patch :complete
+     end
+   end
+ end
+ root "todo_list#index"
 end
